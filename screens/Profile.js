f6d394ee73c9a6a1275550942a9d14060d8773fb
Becca Roeth <roe21002@byui.edu>
2022-12-06 14:04:42 -0700

    Week 12

--- screens/Profile.js
+++ screens/Profile.js
@@ -1,8 +1,9 @@
-import React, { useEffect, useState, } from 'react'; 
-import { StyleSheet, Text, View, Image, SafeAreaView , Share, ScrollView, Button} from 'react-native';
+import React, { useEffect, useState, useRef} from 'react'; 
+import {TouchableOpacity, StyleSheet, Text, View, Image, SafeAreaView , Share, ScrollView, Button} from 'react-native';
 import { Card, CardTitle, CardContent} from 'react-native-material-cards';
 import BarChart from 'react-native-bar-chart';
-import {Camera} from 'expo-camera'; 
+import {Camera, CameraType} from 'expo-camera'; 
+import AsyncStorage from '@react-native-async-storage/async-storage';
 // import Share from 'react-native-share';
 
 
@@ -20,9 +21,23 @@ import {Camera} from 'expo-camera';
 // labels
 // const horizontalData = ['S', 'M', 'T', 'W', 'T', 'F','S'];
 const Profile = (props) => {
+  const[userName, setUserName] = useState("");
+  const[profilePhoto, setProfilePhoto] = useState(null);
+  const[cameraReady, setCameraReady] = useState(false);
+  const cameraRef = useRef(null);
+  
   useEffect(()=>{
     const getUserInfo = async ()=>{
       const cameraPermission = await Camera.requestCameraPermissionsAsync();
+      //setCameraPermission(cameraPermission);
+
+      const userName = await AsyncStorage.getItem('userName');
+      console.log('username', userName);
+      setUserName(userName);
+
+      const profilePhoto = await AsyncStorage.getItem('profilePhoto');
+      console.log('profilephoto', profilePhoto);
+      setProfilePhoto(profilePhoto);
     };
     getUserInfo();
   },[]);
@@ -39,7 +54,29 @@ const Profile = (props) => {
   console.log('Error', error)
       }
     }
+    if (profilePhoto==null){
+      const cameraOptions = {
+        quality:0,
+        exif:false
+      }
+      return (
+        <View style={styles.container}>
+          <Camera type={CameraType.front} style={styles.camera} ref={cameraRef} onCameraReady={()=>{setCameraReady(true)}}>
+            <View style={styles.buttonContainer}>
+              {cameraReady?<TouchableOpacity style={styles.button} onPress={async ()=> {
 
+                const picture = await cameraRef.current.takePictureAsync(cameraOptions);
+                console.log('Picture',picture);
+                await AsyncStorage.setItem('profilePhoto', picture.uri);
+                setProfilePhoto(picture.uri);
+                }}>
+                <Text style={styles.text}>Take Picture</Text>
+              </TouchableOpacity>: null}
+            </View>
+          </Camera>
+        </View>
+      )
+    } else {
   return (
     <SafeAreaView style={{flex: 1}}>
          <Card style={{backgroundColor:'white', borderRadius: 10, margin:20 ,width: 320, shadowColor: "#000",
@@ -48,13 +85,14 @@ shadowOffset: {
 	height: 2,
 },
 shadowOpacity: 0.23,
-shadowRadius: 2.62,
+shadowRadius: 2.62, 
 
 elevation: 4}}>
      <CardContent>
      <Image style={{height: 100, width:100, borderRadius: 75}}
-      source={require('../image/me.jpg')} />
-    <Text style={{marginTop:10,marginBottom:10,fontWeight: 'bold'}}>Sarah Romero</Text>
+      //source = {profilePhoto}
+      source= {require('../image/me.jpg')} />
+    <Text style={{marginTop:10,marginBottom:10,fontWeight: 'bold'}}>{userName}</Text>
 
     <Text style={{marginTop:20,marginBottom:2}}>This Week's progress</Text>
 {/* <BarChart barColor='green' data={data} horizontalData={horizontalData} /> */}
@@ -65,11 +103,33 @@ elevation: 4}}>
     </Card>
  </SafeAreaView>
   );
+}
 };
 export default Profile;
+
 const styles = StyleSheet.create({
-  container:{
-    flex:1,
-    padding:20
-  }
-})
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    padding: 20
+  },
+  camera: {
+    flex: 1,
+  },
+  buttonContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    backgroundColor: 'transparent',
+    margin: 64,
+  },
+  button: {
+    flex: 1,
+    alignSelf: 'flex-end',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: 'white',
+  },
+});
